1dc902d1c5d2a90e6bf95bbe58451a6d044563e6
haseeb-developer <haseeb.developer91@gmail.com>
2024-06-09 22:45:38 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Analytics } from "@vercel/analytics/react";
 import Projects from "./components/Projects";
+import "./App.css";
 
 const App = () => {
   return (
     <div>
+      <h1 className="myproject-title">My Projects</h1>
       <Analytics />
       <Projects />
     </div>
