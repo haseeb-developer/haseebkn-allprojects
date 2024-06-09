25b7499fa4744a8dc3dd335cde9243b941009d77
haseeb-developer <haseeb.developer91@gmail.com>
2024-06-09 22:04:56 +0500

    Updated with analytic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 import Projects from "./components/Projects";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-center pt-4 projects-header">My Projects</h1>
+    <div>
+      <Analytics />
       <Projects />
     </div>
   );
