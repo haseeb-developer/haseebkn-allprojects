803ea2bd2d03fd2ecb3901076fa20c7f30902529
haseebkn <haseeb.devv@gmail.com>
2025-03-01 02:34:17 +0500

    Code updated

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -18,8 +18,8 @@ const Preloader = () => {
     <div className="preloader-overlay">
       <div className="preloader-container">
         <div className="rotating-ring"></div>
-        <div className="glitch-text" data-text="HOLD ON">
-          HOLD ON
+        <div className="glitch-text" data-text="LOADING...">
+          LOADING...
         </div>
       </div>
     </div>
